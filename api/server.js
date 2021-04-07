d9327dcb204e7ac5654a39c090c2d7c58e0f1e08
Chris Burton <chris@fullstackcaveman.com>
2021-04-07 13:18:52 -0600

    Wired server/index

--- api/server.js
+++ api/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const userRouter = require('./users/users-router');
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
+server.use(morgan('dev'));
+server.use(helmet());
+
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 module.exports = server;
